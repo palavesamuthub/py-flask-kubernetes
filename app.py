65f9cb8f8d17b0f5970e6ae92e570560089118c9
Sainithesh <sainithesh21@gmail.com>
2022-07-21 01:12:54 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+import time
+
+PORT = 8000
+MESSAGE = "Hello, world!\n"
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return jsonify({"App Status" : "Running"})
+
+@app.route("/getTime")
+def getTime():
+    seconds = time.time()
+    local_time = time.localtime(seconds)
+    return jsonify({"Year" : local_time.tm_year,"Seconds" : seconds, "Local Time" : local_time})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=PORT)
+
+
